fe9f461ef59e31deee05ed90fb4eb4c681f196ca
Iñigo Sanz Garcia <inigo.sanzgarcia@vixperience.org>
2016-08-10 11:28:11 +0100

    Including MovideDB API parameters

---
+++ config/index.js
@@ -0,0 +1,6 @@
+
+const movieDBUrl = 'https://api.themoviedb.org/3/search/movie?api_key='
+const apikey = 'da0d82f09834a15f289dfa142fbbc2cc'
+const queryParam = '&query='
+
+export const getMovieDBSearchUrl = movieDBUrl + apikey + queryParam
